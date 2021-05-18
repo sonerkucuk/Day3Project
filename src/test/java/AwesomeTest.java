e25b982e47572e72647f0a68292dc67cabf88829
sonerkucuk <sonerkucuk5561@gmail.com>
2021-05-18 11:53:55 +0200

    added new file

---
+++ src/test/java/AwesomeTest.java
@@ -0,0 +1,7 @@
+public class AwesomeTest {
+    //grey in .idea folder means it is ignored
+    //when the color is normal, it is mean it is commited
+    //red mean it is untracked
+    //green mean it is under staging area
+
+}
